a1f2b35c2d74f48d241c29e6bf1cf0e6eb18bd05
João Carvalho <joao.mateussdc@gmail.com>
2024-04-28 11:58:05 -0300

    feat: updated Triangulo.java

--- Lista de Exercícios 2.2/Triangulo.java
+++ Lista de Exercícios 2.2/Triangulo.java
@@ -1,3 +1,54 @@
 public class Triangulo extends FormaGeometrica {
-    
+
+    private float lado1;
+    private float lado2;
+    private float lado3;
+
+    Triangulo(String cor, float lado1, float lado2, float lado3) {
+        this.cor = cor;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    public float getLado1() {
+        return this.lado1;
+    }
+
+    public void setLado1(float new_lado1) {
+        this.lado1 = new_lado1;
+    }
+
+    public float getLado2() {
+        return this.lado2;
+    }
+
+    public void setLado2(float new_lado2) {
+        this.lado2 = new_lado2;
+    }
+
+    public float getLado3() {
+        return this.lado3;
+    }
+
+    public void setLado3(float new_lado3) {
+        this.lado3 = new_lado3;
+    }
+
+    public float calcularArea() {
+        float perimetro = calcularPerimetro();
+        double area = Math.sqrt(perimetro * (perimetro - lado1) * (perimetro - lado2) * (perimetro - lado3));
+
+        return (float) area;
+    }
+
+    public float calcularPerimetro() {
+        float perimetro = lado1 + lado2 + lado3;
+
+        return perimetro;
+    }
+
+    public String toString() {
+        return "Triangulo de cor " + this.cor + ", lado1 " + this.lado1 + ", lado2 " + this.lado2 + " e lado3 " + this.lado3;
+    }
 }
