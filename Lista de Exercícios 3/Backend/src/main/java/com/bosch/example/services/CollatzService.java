07d616788f619a0c11a7756277da5c3e023aeaf8
João Carvalho <joao.mateussdc@gmail.com>
2024-06-23 19:11:24 -0300

    fix: deleting some arquives

--- Lista de Exercícios 3/Backend/src/main/java/com/bosch/example/services/CollatzService.java
+++
@@ -1,5 +0,0 @@
-package com.bosch.example.services;
-
-public interface CollatzService {
-    Integer collatz(int current, int step);
-}
--- Lista de Exercícios 3/Backend/src/main/java/com/bosch/example/services/ReverseService.java
+++
@@ -1,6 +0,0 @@
-package com.bosch.example.services;
-
-public interface ReverseService {
-    String reverse(String word);
-    Boolean palindrome(String word, String reverseWord);
-}
